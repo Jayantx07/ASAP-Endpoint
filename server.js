f9c694d30e6eb3fb1e20c2f5a0b579fe77f1da23
Jayantx07 <jayant.kumawat.s64@kalvium.community>
2025-02-27 12:05:43 +0530

    Updated all

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Define /ping route
+app.get('/ping', (req, res) => {
+    res.json({ message: 'pong' });
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
